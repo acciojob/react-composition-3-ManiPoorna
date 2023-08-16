c2c84c292e9d0ba062f631eb652dc7de480ffe95
Mani Poorna <ndmprao2001@gmail.com>
2023-08-16 12:13:23 +0530

    Versin 1.2

--- src/components/Rc3.js
+++ src/components/Rc3.js
@@ -10,13 +10,13 @@ const Rc3 = ()=>{
         <div>
             <div className="top">
                 {
-                    topHover && <h4>This is a tooltip</h4>
+                    topHover && <h2>This is a tooltip</h2>
                 }
-                <h2 onMouseOver={()=>setTopHover(true)} onMouseLeave={()=>setTopHover(false)} className="tootip">Hover over me</h2>
+                <p onMouseOver={()=>setTopHover(true)} onMouseLeave={()=>setTopHover(false)} className="tootip">Hover over me</p>
             </div>
             <div className="bottom">
                 {
-                    bottomHover && <h4>This is another tooltip</h4>
+                    bottomHover && <h2>This is another tooltip</h2>
                 }
                 <p onMouseOver={()=>setBottomHover(true)} onMouseLeave={()=>setBottomHover(false)} className="tooltiptext">Hover over me to see another tooltip</p>
             </div>
